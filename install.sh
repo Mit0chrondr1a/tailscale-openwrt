2a92da6571d34e7b6c386fe9c740bbb5afa6fb2d
MitoChrondria <12169214+Mit0chrondr1a@users.noreply.github.com>
2024-11-19 16:46:37 +0800

    init NAT router as exit node to install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,69 @@
 #!/bin/sh
-arch_=`uname -m`
-if [ "$arch_" == "i386" ]; then
-arch=386
-elif [ "$arch_" == "x86_64" ]; then
-arch=amd64
-elif [ "$arch_" == "armv7l" ]; then
-arch=arm
-elif [ "$arch_" == "aarch64" ]; then
-arch=arm64
-elif [ "$arch_" == "armv8l" ]; then
-arch=arm64
-elif [ "$arch_" == "geode" ]; then
-arch=geode
-elif [ "$arch_" == "mips" ]; then
-endianness=`echo -n I | hexdump -o | awk '{ print (substr($2,6,1)=="1") ? "le" : ""; exit }'`
-elif [ "$arch_" == "riscv64" ]; then
-arch=riscv64
-else
-echo "The architecture of the current machine is ${arch_}${endianness} , the schema code built into the script may not match your machine, please leave a comment on this issue so that the author can modify the script in time: https://github.com/CH3NGYZ/tailscale-openwrt/issues/6"
-exit 1
-fi
+arch_=$(uname -m)
+endianness=""
+
+case "$arch_" in
+    i386)
+        arch=386
+        ;;
+    x86_64)
+        arch=amd64
+        ;;
+    armv7l)
+        arch=arm
+        ;;
+    aarch64 | armv8l)
+        arch=arm64
+        ;;
+    geode)
+        arch=geode
+        ;;
+    mips)
+        arch=mips
+        endianness=$(echo -n I | hexdump -o | awk '{ print (substr($2,6,1)=="1") ? "le" : "be"; exit }')
+        ;;
+    riscv64)
+        arch=riscv64
+        ;;
+    *)
+        echo "INSTALL: --------------------------------------------"
+        echo "当前机器的架构是 [${arch_}${endianness}]"
+        echo "脚本内置的架构代码可能有误,不符合您的机器"
+        echo "请在这个issue留下评论以便作者及时修改脚本"
+        echo "https://github.com/CH3NGYZ/tailscale-openwrt/issues/6"
+        echo "------------------------------------------------------"
+        exit 1
+        ;;
+esac
 
 if [ -e /tmp/tailscaled ]; then
-        echo "Residue exists, uninstall it, restart your machine and try again"
-        exit 1
+    echo "INSTALL: ------------------"
+    echo "存在残留, 请卸载并重启后重试"
+    echo "卸载命令: wget -qO- https://ghproxy.net/https://raw.githubusercontent.com/CH3NGYZ/tailscale-openwrt/chinese_mainland/uninstall.sh | sh"
+    echo "---------------------------"
+    exit 1
 fi
 
-# opkg update
-opkg install libustream-openssl ca-bundle kmod-tun
-echo "If the package fails to be installed, manually run the following command to install the package. If the problem persists, manually locate the cause:"
-echo "opkg install libustream-openssl"
-echo "opkg install ca-bundle"
-echo "opkg install kmod-tun"
-echo "All three packages are indispensable"
+opkg update
+
+# 检查并安装包
+required_packages="libustream-openssl ca-bundle kmod-tun coreutils-timeout"
+for package in $required_packages; do
+    # 检查包是否已安装
+    if ! opkg list-installed | grep -q "$package"; then
+        echo "INSTALL: 包 $package 未安装，开始安装..."
+        opkg install "$package"
+        if [ $? -ne 0 ]; then
+            echo "INSTALL: 安装 $package 失败，跳过该包，如果无法正常运行 tailscale，请排查是否需要手动安装该包"
+            continue
+        else
+            echo "INSTALL: 包 $package 安装成功"
+        fi
+    else
+        echo "INSTALL: 包 $package 已安装，跳过"
+    fi
+done
+
 
 # 下载安装包
 wget --tries=5 -c -t 60 https://raw.githubusercontent.com/CH3NGYZ/tailscale-openwrt/main/tailscale-openwrt.tgz
@@ -70,6 +101,5 @@ while true; do
 done
 
 echo "If the login fails, check the background service running status by running /etc/init.d/tailscaled status"
-tailscale up
-tailscale up
+tailscale up --accept-dns=false --advertise-exit-node
 echo "The current machine architecture is arch_:${arch_}${endianness} | arch:${arch} . If it works successfully, leave a comment on this issue so that the author can revise the documentation in time: https://github.com/CH3NGYZ/tailscale-openwrt/issues/6"
